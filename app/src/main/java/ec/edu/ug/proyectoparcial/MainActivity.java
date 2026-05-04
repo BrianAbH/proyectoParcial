154a941b9fb0483053d018a6e427a8cabaea59f6
BrianAb <bryancarranzafreijo@gmail.com>
2026-05-03 22:44:39 -0500

    ajustes de tamaños

--- app/src/main/java/ec/edu/ug/proyectoparcial/MainActivity.java
+++ app/src/main/java/ec/edu/ug/proyectoparcial/MainActivity.java
@@ -1,6 +1,9 @@
 package ec.edu.ug.proyectoparcial;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //Elementos
+    Button btnUg;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //Referencias
+        btnUg = findViewById(R.id.btnUriUg);
+
+
+        //Intent implicito
+        btnUg.setOnClickListener(v->{
+            Uri urlUg = Uri.parse("https://www.ug.edu.ec/");
+            Intent urlI = new Intent(Intent.ACTION_VIEW,urlUg);
+            startActivity(urlI);
+
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
