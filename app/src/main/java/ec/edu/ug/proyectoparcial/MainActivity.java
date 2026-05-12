56814929876b23f91b0080d75cbeb2587b5d4ff9
BrianAb <bryancarranzafreijo@gmail.com>
2026-05-11 19:38:51 -0500

    Preferencias_añadidas

--- app/src/main/java/ec/edu/ug/proyectoparcial/MainActivity.java
+++ app/src/main/java/ec/edu/ug/proyectoparcial/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,11 +17,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    //Elementos
-    Button btnUg;
-    CardView cvPreferencias;
 
-    TextView tvSaludo;
+    private static String NOMBRE;
+    //Elementos
+    private Button btnUg;
+    private CardView cvPreferencias;
+
+    private TextView tvSaludo;
+
+    private SharedPreferences prfs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +33,26 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        iniciarComponentes();
+        obtenerPreferenciaNombre();
+        iniciarIntent();
+        saludoInicial();
+    }
+
+    private void iniciarComponentes(){
         //Referencias
         btnUg = findViewById(R.id.btnUriUg);
         cvPreferencias = findViewById(R.id.cvPreferencias);
         tvSaludo = findViewById(R.id.tvSaludo);
-        SharedPreferences prfs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+    }
 
-        String nombre = prfs.getString("Nombre","");
-
-        tvSaludo.setText("Hola " + nombre);
-
-
-
+    private void iniciarIntent(){
         //Intent implicito
         btnUg.setOnClickListener(v->{
             Uri urlUg = Uri.parse("https://www.ug.edu.ec/");
@@ -52,11 +65,24 @@ public class MainActivity extends AppCompatActivity {
             Intent iPreferencias = new Intent(this, PreferencesActivity.class);
             startActivity(iPreferencias);
         });
+    }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+    private void obtenerPreferenciaNombre(){
+        prfs = getSharedPreferences("MIS_PREFERENCIAS", Context.MODE_PRIVATE);
+        NOMBRE = prfs.getString("NOMBRE","");
+
+        if (NOMBRE.isEmpty()){
+            tvSaludo.setText(getString(R.string.msj_bienvenida_pordefecto));
+        }else{
+            tvSaludo.setText(getString(R.string.msjBienvenida, NOMBRE));
+        }
+    }
+
+    private void saludoInicial(){
+        boolean estado = prfs.getBoolean("ESTADO",false);
+        if (estado){
+            Toast.makeText(this, getString(R.string.msjBienvenida, NOMBRE), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/ec/edu/ug/proyectoparcial/PreferencesActivity.java
+++ app/src/main/java/ec/edu/ug/proyectoparcial/PreferencesActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -19,55 +20,102 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class PreferencesActivity extends AppCompatActivity {
 
-    ImageButton ivVolver;
-    EditText etNombre, etCurso;
-    Button btnGuardarPrfs;
+    private static String MIS_PREFERENCIAS = "MIS_PREFERENCIAS";
+    private static String NOMBRE = "NOMBRE";
+    private static String CURSO = "CURSO";
+    private static String ESTADO = "ESTADO";
+
+    private ImageButton ivVolver;
+    private EditText etNombre, etCurso;
+    private Button btnGuardarPrfs, btnLimpiar;
+    private Switch swSaludo;
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_preferences);
-
-        //Referencias
-        ivVolver = findViewById(R.id.ivVolver);
-        etNombre = findViewById(R.id.etNombre);
-        etCurso = findViewById(R.id.etCurso);
-        btnGuardarPrfs = findViewById(R.id.btnGuardarPrfs);
-        SharedPreferences prefs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
-
-        ivVolver.setOnClickListener(v->{
-            Intent iVolver = new Intent(this, MainActivity.class);
-            startActivity(iVolver);
-        });
-
-        btnGuardarPrfs.setOnClickListener(v->{
-            if (etNombre.getText().toString().isEmpty() || etCurso.getText().toString().isEmpty()){
-                etNombre.setError("No puede quedar vacio");
-                etCurso.setError("No puede quedar vacio");
-            }else{
-                guardarPrfs();
-            }
-        });
-
-        String nombreP = prefs.getString("Nombre", "");
-        etNombre.setText(nombreP);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.prefencias), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        preferences = getSharedPreferences(MIS_PREFERENCIAS, Context.MODE_PRIVATE);
 
+        iniciarComponenetes();
+        validarCampos();
+        volverMenu();
+        llenarCampos();
+        guardarEstado();
     }
 
-    public void guardarPrfs(){
-        SharedPreferences prefs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("Nombre", etNombre.getText().toString().trim());
-        editor.putString("Curso", etCurso.getText().toString().trim());
-        editor.commit();
-        Toast.makeText(this,"Datos Guardados Correctamente", Toast.LENGTH_LONG).show();
+    private void iniciarComponenetes(){
+        ivVolver = findViewById(R.id.ivVolver);
+        etNombre = findViewById(R.id.etNombre);
+        etCurso = findViewById(R.id.etCurso);
+        btnGuardarPrfs = findViewById(R.id.btnGuardarPrfs);
+        swSaludo = findViewById(R.id.swSaludo);
+        btnLimpiar = findViewById(R.id.btnLimpiar);
+
+        btnLimpiar.setOnClickListener(v->eliminarPreferencias());
     }
+
+    private void validarCampos(){
+        btnGuardarPrfs.setOnClickListener(v->{
+            if (etNombre.getText().toString().isEmpty() || etCurso.getText().toString().isEmpty()){
+                etNombre.setError(getString(R.string.error_nombre));
+                etCurso.setError(getString(R.string.error_curso));
+            }else{
+                guardarPrfs();
+            }
+        });
+    }
+
+    private void guardarPrfs(){
+        preferences.edit()
+        .putString(NOMBRE, etNombre.getText().toString().trim())
+        .putString(CURSO, etCurso.getText().toString().trim())
+        .apply();
+        Toast.makeText(this,R.string.msj_toast, Toast.LENGTH_LONG).show();
+    }
+
+    private void guardarEstado(){
+        swSaludo.setOnCheckedChangeListener((Switch, isChecked) -> {
+            if (isChecked){
+                preferences.edit()
+                        .putBoolean(ESTADO, true).apply();
+            }else{
+                preferences.edit()
+                        .putBoolean(ESTADO, false).apply();
+            }
+        });
+    }
+
+    private void volverMenu(){
+        ivVolver.setOnClickListener(v->{
+            Intent iVolver = new Intent(this, MainActivity.class);
+            startActivity(iVolver);
+        });
+    }
+
+
+    private void llenarCampos(){
+        String nombre = preferences.getString("NOMBRE","");
+        String curso = preferences.getString("CURSO","");
+        boolean estado = preferences.getBoolean("ESTADO", false);
+        etNombre.setText(nombre);
+        etCurso.setText(curso);
+        swSaludo.setChecked(estado);
+    }
+
+    private void eliminarPreferencias(){
+        preferences.edit().clear().apply();
+        etNombre.setText("");
+        etCurso.setText("");
+        Toast.makeText(this, R.string.btn_limpiar_texto, Toast.LENGTH_SHORT).show();
+    }
+
 
 }
